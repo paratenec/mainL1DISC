225a3eb378b6022e02a8c9bf9f9c9c713577613a
paratenec <paratenec@stu.otc.school.nz>
2020-07-28 13:17:30 +1200

    First two if commands

--- Program Assessment.py
+++ Program Assessment.py
@@ -3,3 +3,13 @@ print("Hello, World")
 day1 = str(input("How are you feeling today?"))
 
 print("Today you were feeling{}".format(day1))
+
+if day1 = happy:
+    print("That is great to hear")
+    print("What made it so good?")
+
+
+if day1 = sad:
+    print("That's no good")
+    print("Could you please explain further?")
+
