b5b17987036291357f1523700f089ab5bac1ebb5
paratenec <paratenec@stu.otc.school.nz>
2020-07-28 13:28:39 +1200

    if command edit

--- Program Assessment.py
+++ Program Assessment.py
@@ -1,15 +1,15 @@
 print("Hello, World")
 
-day1 = str(input("How are you feeling today?"))
+day1 = str(input("How are you feeling today?".strip().lower()))
 
 print("Today you were feeling{}".format(day1))
 
-if day1 = happy:
+if day1 == happy:
     print("That is great to hear")
     print("What made it so good?")
 
 
-if day1 = sad:
+if day1 == sad:
     print("That's no good")
     print("Could you please explain further?")
 
